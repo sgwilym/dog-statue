6d32e642b6749dcd20bd0ffc30a3989dba00f26f
Samuel Gwilym <sam@comique.co>
2016-08-01 11:46:10 +0200

    Rudimentary styling for vignette

--- Vignette/SlideViewer.elm
+++ Vignette/SlideViewer.elm
@@ -10,7 +10,16 @@ import Vignette.Models exposing (..)
 view : Vignette -> Html Msg
 view vignette =
     button
-        [ onClick Progress, style [ ( "z-index", "2" ), ( "position", "absolute" ) ] ]
+        [ onClick Progress
+        , style
+            [ ( "top", "30px" )
+            , ( "left", "30px" )
+            , ( "right", "30px" )
+            , ( "bottom", "30px" )
+            , ( "z-index", "2" )
+            , ( "position", "absolute" )
+            ]
+        ]
         [ case currentSlide vignette of
             Picture url ->
                 img [ src url ] []
